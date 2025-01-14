af86e42bf557c32470c37bc08ab03b401fef7dbb
Pratham-Mishra01 <prathamnmishra@gmail.com>
2025-01-14 17:43:15 +0530

    Time: 83 ms (46.73%), Space: 81.3 MB (16.61%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         if(n==0) return 0;
         int cnt;
-        unordered_set<int> st;
+        unordered_set<int> st;  //we use set data structure to not tamper with array
         int longest=1;
         for(int i=0;i<n;i++)
         {
@@ -16,7 +16,7 @@ public:
             {
                 cnt=1;
                 int x=it;
-                while(st.find(x+1)!=st.end()){
+                while(st.find(x+1)!=st.end()){   //possible mistake: using if statement instead of while
                     cnt++;
                     x++;
                 }
