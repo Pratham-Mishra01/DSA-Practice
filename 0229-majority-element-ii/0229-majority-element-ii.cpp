1c62e8527531576176a265c062bb8138b4e57949
Pratham-Mishra01 <prathamnmishra@gmail.com>
2025-01-25 17:40:32 +0530

    Time: 0 ms (100%), Space: 19.9 MB (60.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int threshold=n/3;
-        map<int,int> mpp;
-        vector<int> ans;
-        for(int i=0;i<n;i++)
-        {
-            mpp[nums[i]]++;
+        int cnt1=0,cnt2=0;
+        int el1=INT_MIN, el2=INT_MIN;
 
-        }
-        for(const auto& i: mpp)
+        for(int i=0;i<nums.size();i++)
         {
-            if(i.second>threshold)
-                ans.push_back(i.first);
+            if(cnt1==0 && nums[i]!=el2)
+            {el1=nums[i];
+                cnt1=1;
+            }
+            else if(cnt2==0 && nums[i]!=el1)
+            {
+                el2=nums[i];
+                cnt2=1;
+            }
+            else if(nums[i]==el1)
+            cnt1++;
+            else if(nums[i]==el2)
+            cnt2++;
+
+            else
+            {
+                cnt1--;
+                cnt2--;
+
+            }
         }
-        return ans;
+
+            vector<int>ans;
+            int cnt11=0,cnt22=0;
+            for(int i=0;i<nums.size();i++)
+            {
+                if(nums[i]==el1) cnt11++;
+                if(nums[i]==el2) cnt22++;
+
+
+            }
+            int threshold=(int)(nums.size()/3)+1;
+            if(cnt11>=threshold) ans.push_back(el1);
+            if(cnt22>=threshold) ans.push_back(el2);
+
+            sort(ans.begin(),ans.end());
+            return ans;
         
     }
 };
